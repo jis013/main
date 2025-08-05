8f14b5dd58ae8903ddad3b3fb95046a4c1806401
jis013 <147174253+jis013@users.noreply.github.com>
2025-08-05 17:44:09 +0530

    Add files via upload

---
+++ lightdark.js
@@ -0,0 +1,11 @@
+function light() {
+      h1.innerHTML = "Light Mode"
+      body.style.background = "white"
+      body.style.color = "black"
+}
+
+function dark() {
+      h1.innerHTML = "Dark Mode"
+      body.style.background = "black"
+      body.style.color = "white"
+} 
